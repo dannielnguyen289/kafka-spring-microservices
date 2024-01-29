90509569d9a1ca2a1e9ebfe4c64aca86e56b0149
Danniel Nguyen <dannielnguyen289@gmail.com>
2024-01-29 16:24:42 +0700

    Initial project

--- deployment/kafka_topic.sh
+++ deployment/kafka_topic.sh
@@ -1,1 +1,5 @@
-docker run -it --rm bitnami/kafka:3.6 kafka-topics.sh --list  --bootstrap-server 10.16.1.3:9095
+docker run -it --rm bitnami/kafka:3.6 kafka-topics.sh --list --bootstrap-server 10.16.1.3:9095
+
+docker run -it --rm bitnami/kafka:3.6 kafka-topics.sh --create --topic sample-topic --partitions 12 --replication-factor 3 --bootstrap-server 10.16.1.3:9095
+
+docker run -it --rm bitnami/kafka:3.6 kafka-topics.sh --describe --topic sample-topic --bootstrap-server 10.16.1.3:9095
